ce7ff8571a070c19f49fef07348c5ce6040a028b
kugnfu-frog <kungfu.master0128@protonmail.com>
2021-01-11 09:54:44 +0800

    contact list finished

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import parser from 'fast-xml-parser';
+import he from 'he';
 
 class LoginButton extends React.Component {
   constructor(props) {
@@ -18,16 +21,54 @@ class LoginButton extends React.Component {
 
     let user = googleUser.getBasicProfile();
     let id_token = googleUser.getAuthResponse().id_token;
+    let response = googleUser.getAuthResponse();
+    let access_token = window.gapi.auth2.getAuthInstance().currentUser.get().Bc.access_token;
 
     console.log('google user obj', user);
     console.log('google_id_token', id_token);
-    // plus any other logic here
+    console.log('google_auth_response', response);
+    console.log('googleUser', access_token);
+    // plus any other logic here'
+
+    axios.get(`https://www.google.com/m8/feeds/contacts/${user.tu}/full`, {
+      headers: {
+        Authorization: response.token_type + ' ' + access_token,
+        'GData-Version': '3.0',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(({data}) => {
+      var options = {
+        attributeNamePrefix : "",
+        attrNodeName: "attr", //default is 'false'
+        textNodeName : "#text",
+        ignoreAttributes : false,
+        ignoreNameSpace : false,
+        allowBooleanAttributes : false,
+        parseNodeValue : true,
+        parseAttributeValue : true,
+        trimValues: true,
+        cdataTagName: "__cdata", //default is 'false'
+        cdataPositionChar: "\\c",
+        parseTrueNumberOnly: false,
+        arrayMode: false, //"strict"
+        attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+        tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+        stopNodes: ["parse-me-as-string"]
+      };
+      
+      if( parser.validate(data) === true) { //optional (it'll return an object in case it's not valid)
+          var jsonObj = parser.parse(data,options);
+          console.log(jsonObj);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
   };
 
   renderGoogleLoginButton = () => {
     console.log('rendering google signin button');
     window.gapi.signin2.render('my-signin2', {
-      scope: 'profile email',
+      scope: 'profile email https://www.googleapis.com/auth/contacts.readonly',
       width: 250,
       height: 40,
       longtitle: true,
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const credentials = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET
+}
